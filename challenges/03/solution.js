7d4ced7b0e9d08ebbdb2ab512487951944ebbe3c
Dani <danizt96@gmail.com>
2022-06-11 15:24:34 +0200

    Refactor solution challenge 03

--- challenges/03/solution.js
+++ challenges/03/solution.js
@@ -1,55 +1,62 @@
 function isValid(letter) {
-  let goodClosedParentheses = true
-  let hasBrackets = false
+  let wellFormedLetter = true
   let openedParentheses = false
-
+  let parenthesesHasContent = false
   const arrayFromLetter = Array.from(letter)
 
-  arrayFromLetter.forEach((char, index) => {
-    if (goodClosedParentheses) {
-      if (char === '(') {
-        openedParentheses = true
+  for (const char of arrayFromLetter) {
+    // Validate only one opening parenthesis active at a time
+    if (char === '(') {
+      if (openedParentheses) {
+        wellFormedLetter = false
+        return false
       }
-
-      // Validate well closed parentheses and not empty value
-      if (char === ')') {
-        if (openedParentheses) {
-          const emptyParentheses = arrayFromLetter[index - 1] === '('
-          if (!emptyParentheses) {
-            goodClosedParentheses = true
-            openedParentheses = false
-          } else {
-            goodClosedParentheses = false
-          }
-        } else {
-          goodClosedParentheses = false
-        }
-      }
-
-      // Validate brackets and only one item without spaces
-      if (
-        openedParentheses &&
-        (char === '{' ||
-          char === '[' ||
-          char === '}' ||
-          char === ']' ||
-          char === ' ')
-      ) {
-        hasBrackets = true
-      }
+      openedParentheses = true
+      continue
     }
-  })
 
-  return goodClosedParentheses && !hasBrackets
+    // Validate brackets and only one item without spaces
+    if (
+      openedParentheses &&
+      (char === '{' ||
+        char === '[' ||
+        char === '}' ||
+        char === ']' ||
+        char === ' ')
+    ) {
+      wellFormedLetter = false
+      return false
+    }
+
+    // Validate well closed parentheses and not empty value
+    if (char === ')') {
+      if (!openedParentheses || !parenthesesHasContent) {
+        wellFormedLetter = false
+        return false
+      }
+      openedParentheses = false
+      parenthesesHasContent = false
+    }
+
+    if (openedParentheses === true && char !== ')') {
+      parenthesesHasContent = true
+    }
+
+    wellFormedLetter = true
+  }
+
+  return wellFormedLetter
 }
 
-console.log('✅ -> ' + isValid('(balón)')) // -> ✅
-console.log('❌ -> ' + isValid('(balón))')) // -> ❌
+console.log('✅ -> ' + isValid('(balón)'))
+console.log('❌ -> ' + isValid('(balón))'))
 
-console.log('✅ -> ' + isValid('bici coche (balón) bici coche peluche')) // -> ✅
-console.log('✅ -> ' + isValid('(muñeca) consola bici')) // ✅
+console.log('✅ -> ' + isValid('bici coche (balón) bici coche peluche'))
+console.log('✅ -> ' + isValid('(muñeca) consola bici'))
 
-console.log('❌ -> ' + isValid('bici coche (balón bici coche')) // -> ❌
-console.log('❌ -> ' + isValid('peluche (bici [coche) bici coche balón')) // -> ❌
-console.log('❌ -> ' + isValid('(peluche {) bici')) // -> ❌
-console.log('❌ -> ' + isValid('() bici')) // ❌
+console.log('❌ -> ' + isValid('bici coche (balón bici coche'))
+console.log('❌ -> ' + isValid('peluche (bici [coche) bici coche balón'))
+console.log('❌ -> ' + isValid('(peluche {) bici'))
+console.log('❌ -> ' + isValid('() bici'))
+
+console.log('❌ -> ' + isValid('(a() bici (a)'))
