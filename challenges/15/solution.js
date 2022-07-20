e0611d1bf362d866637864661d27ae07cf5cc27a
Dani <danizt96@gmail.com>
2022-07-20 15:37:34 +0200

    Challenge 15 solved

--- challenges/15/solution.js
+++ challenges/15/solution.js
@@ -1,1 +1,26 @@
-// Solution challenge 15
+function checkSledJump(heights) {
+  let estado = true // true = subiendo, false = bajando
+  let result = true
+
+  heights.forEach((height, index) => {
+    if (index === 0) return ''
+    let prevHeight = heights[index - 1]
+
+    if ((height > prevHeight && !estado) || height === prevHeight) {
+      result = false
+    }
+
+    if (height < prevHeight) estado = false
+  })
+
+  return result && !estado
+}
+
+console.log(checkSledJump([1, 2, 3, 2, 1])) // true: sube y baja de forma estricta
+console.log(checkSledJump([0, 1, 0])) // -> true: sube y baja de forma estricta
+console.log(checkSledJump([0, 3, 2, 1])) // -> true: sube y baja de forma estricta
+console.log(checkSledJump([0, 1000, 1])) // -> true: sube y baja de forma estricta
+
+console.log(checkSledJump([2, 4, 4, 6, 2])) // false: no sube de forma estricta
+console.log(checkSledJump([1, 2, 3])) // false: sólo sube
+console.log(checkSledJump([1, 2, 3, 2, 1, 2, 3])) // false: sube y baja y sube... ¡no vale!
